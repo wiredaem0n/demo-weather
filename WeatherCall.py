ee5f94b82191cbff180a54cdf1d7770ab570eee2
ericv <eric.vargas@lazada.com>
2018-06-17 16:30:28 +0800

    Added options

--- WeatherCall.py
+++ WeatherCall.py
@@ -1,23 +1,39 @@
 #!/usr/bin/env python
-import urllib2
-import time
-import json
 
-#####Openweathermap.org Accumulated DATA Precipitation API URLS ################################################################################################################
+##### Openweathermap.org Accumulated Data Precipitation API url ################################################################################################################
 #PAID   API url  http://history.openweathermap.org/data/2.5/history/accumulated_precipitation?id={city ID}&start={start}&end={end}													
 #Sample API url http://samples.openweathermap.org/data/2.5/history/accumulated_precipitation?id=4887398&start=1505336400&end=1505941200&appid=b1b15e88fa797225412429c1c50c122a1"    
 ################################################################################################################################################################################### 
-baseurl		= "http://history.openweathermap.org/data/2.5/history/accumulated_precipitation?id=4887398&appid=6d08d471221ed4f8b902fda13c58d276&"
-sampleurl 	= "http://samples.openweathermap.org/data/2.5/history/accumulated_precipitation?id=4887398&appid=6d08d471221ed4f8b902fda13c58d276&"
-api_key		= "6d08d471221ed4f8b902fda13c58d276"
-city_id		= "4887398" 
-sdate 		= int(time.time())
-edate 		= sdate - (86400 * 5)	
+
+import urllib2
+import time
+import json
+import sys
+import datetime
+from optparse import OptionParser
+
+options = OptionParser(usage='%prog server [options]', description='Openweathermap API call Sample')
+options.add_option('-k', '--apikey', type='str', help='API Key [Required]')
+options.add_option('-n', '--days', type='int', default=5, help='Number of days (default:5)')
+options.add_option('--cid', type='str', default="4887398", help='City ID to query (default:Chicago)')
+opts, args = options.parse_args()
 
 
-#API Request
-fullurl = sampleurl + "&start=" + str(sdate) + "&end=" + str(edate)
+if not opts.apikey :
+  sys.exit(options.print_help())
 
+
+baseurl		= "http://history.openweathermap.org/data/2.5/history/accumulated_precipitation?"
+sampleurl 	= "http://samples.openweathermap.org/data/2.5/history/accumulated_precipitation?"
+api_key		= opts.apikey
+city_id		= opts.cid
+ndays		= opts.days
+sdate 		= int(time.time()) # in unix time
+edate 		= sdate - (86400 * ndays)	
+fullurl		= sampleurl + "id=" + city_id + "&start=" + str(sdate) + "&end=" + str(edate) + "&appid=" + api_key
+
+#API Call
+print "Requesting the last " + str(ndays) + " days."
 
 try:
 	headers = {}
